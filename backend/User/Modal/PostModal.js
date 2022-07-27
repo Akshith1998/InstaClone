ad8c0181b6a3a70999a9b2ad781ed8edb58c6554
Akshith1998 <akshithpatluri@gmail.com>
2022-07-27 14:59:08 +0530

    sec

--- backend/User/Modal/PostModal.js
+++ backend/User/Modal/PostModal.js
@@ -1,13 +1,16 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const PostSchema=new mongoose.Schema({
-    Author:String,
-    Location:String,
-    Description:String,
-    Image:String
-},{
-    timestamps:true
-})
+const PostSchema = new mongoose.Schema(
+  {
+    Author: { type: String, required: true },
+    Location: String,
+    Description: String,
+    Image: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const PostModal=mongoose.model("Post",PostSchema);
-module.exports=PostModal;
+const PostModal = mongoose.model("Post", PostSchema);
+module.exports = PostModal;
